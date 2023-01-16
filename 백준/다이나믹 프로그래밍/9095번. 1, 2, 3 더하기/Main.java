dd1ce47fffaedf7b88ba7133ce3caf179b79d8ce
sjunh812 <sjunh812@naver.com>
2023-01-16 18:14:33 +0900

    다이나믹 프로그래밍
    - 백준 문제 풀기

---
+++ 백준/다이나믹 프로그래밍/9095번. 1, 2, 3 더하기/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+  public static void main(String[] args) throws Exception {
+    new Main().solution();
+  }
+
+  public void solution() throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int t = Integer.parseInt(br.readLine());
+
+    for (int tc = 0; tc < t; tc++) {
+      int n = Integer.parseInt(br.readLine());
+
+      int[] dp = new int[11];
+      dp[1] = 1;
+      dp[2] = 2;
+      dp[3] = 4;
+
+      for (int i = 4; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+      }
+
+      System.out.println(dp[n]);
+    }
+  }
+}
